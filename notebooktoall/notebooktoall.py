7c98779757a22948a0083f5c281b358cef654d91
discdiver <jeffmshale@gmail.com>
2019-02-10 15:59:04 -0500

    modularizes code and reduces hard coded params

--- notebooktoall/notebooktoall.py
+++ notebooktoall/notebooktoall.py
@@ -1,39 +1,45 @@
+from urllib.request import urlopen
+import nbformat
+from traitlets.config import Config
+
+# 1. Import the exporter
+from nbconvert import HTMLExporter
+from nbconvert import PythonExporter
+from nbconvert.writers import FilesWriter
+
 def build_files(
     url="http://jakevdp.github.com/downloads/notebooks/XKCD_plots.ipynb",
-    notebook_name="my_converted_nb"
+    notebook_name="my_converted_nb",
+    exports=["html", "py"]
 ):
-    from urllib.request import urlopen
-    import nbformat
-    from traitlets.config import Config
-
-    # 1. Import the exporter
-    from nbconvert import HTMLExporter
-    from nbconvert import PythonExporter
-    from nbconvert.writers import FilesWriter
 
     try:
         response = urlopen(url).read().decode()
-        response[0:5] + '...'
-        #print(response)
     except Exception as e:
         print(f"There was a problem: {e}")
 
     try:
-        # Read a notebook from a string and return the NotebookNode object as the given version.
-        jake_notebook = nbformat.reads(response, as_version=4)
-        jake_notebook.cells[0]
-
-        # export and write html file.
-        html_exporter = HTMLExporter()
-        html_exporter.template_file = 'basic'
-        (body, resources) = html_exporter.from_notebook_node(jake_notebook)
-        write_file = FilesWriter()
-        write_file.write(output=body, resources=resources, notebook_name=notebook_name)
-
-        # export and write html file.
-        py_exporter = PythonExporter()
-        (body, resources) = py_exporter.from_notebook_node(jake_notebook)
-        write_file = FilesWriter()
-        write_file.write(output=body, resources=resources, notebook_name=notebook_name)
+        # Read a notebook from a string and return the NotebookNode object
+        nb = nbformat.reads(response, as_version=4)
+        exper(exports, nb, notebook_name)
     except Exception as e:
         print(f"There was a problem exporting or writing: {e}")
+    return None
+
+def exper(exps, nb_node, file_name):
+    """
+    export and write files
+    exports(list of strings): name of valid nbconvert exporters
+    """
+    # TODO: Check that exports are valid types, let input valid types
+
+    # export and write file.
+    for exp in exps:
+        if exp == "html":
+            exporter = HTMLExporter()
+        elif exp == "py":
+            exporter = PythonExporter()
+        (body, resources) = exporter.from_notebook_node(nb_node)
+        write_file = FilesWriter()
+        write_file.write(output=body, resources=resources, notebook_name=file_name)
+    return None
