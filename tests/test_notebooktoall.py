b82eb0fa8ac4a8374039908c563f1cd1906bd220
discdiver <jeffmshale@gmail.com>
2019-02-10 10:42:13 -0500

    make first test

--- tests/test_notebooktoall.py
+++ tests/test_notebooktoall.py
@@ -4,22 +4,22 @@
 """Tests for `notebooktoall` package."""
 
 import pytest
-
+import requests
+import os
 
 from notebooktoall import notebooktoall
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
+#@pytest.fixture
+#def import_export():
+#    """Sample pytest fixture.
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
+#    See more at: http://doc.pytest.org/en/latest/fixture.html
+#    """
     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
 
 
-def test_content(response):
+def test_ouptuts_html_file():
     """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+
+    assert os.path.exists('mynb.html')
