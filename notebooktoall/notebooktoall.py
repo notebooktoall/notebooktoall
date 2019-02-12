420109b9492599b6f0110dd75a4a4a57ab3d6e2d
discdiver <jeffmshale@gmail.com>
2019-02-11 23:10:16 -0500

    tox passes linting

--- notebooktoall/notebooktoall.py
+++ notebooktoall/notebooktoall.py
@@ -1,16 +1,22 @@
+"""Transform Jupyter Notebook to executable python script and/or html file."""
+
 from urllib.request import urlopen
 import nbformat
-from traitlets.config import Config
 from nbconvert import HTMLExporter, PythonExporter
 from nbconvert.writers import FilesWriter
 
+
 def get_notebooks(ipynb_file):
     """
-    gets a Jupyter notebook from a url or directory and converts to a NotebookNode object
-    ipynb_file (str): url or path of notebook
-    returns: NotebookNode object
-    """
+    Convert Jupyter notebook from url or directory to a NotebookNode object.
 
+    Args:
+        ipynb_file (str): url or path of notebook
+
+    Returns:
+        NotebookNode object
+
+    """
     try:
         if "http" in ipynb_file:
             ipynb_file = urlopen(ipynb_file).read().decode()
@@ -21,15 +27,20 @@ def get_notebooks(ipynb_file):
     except Exception as e:
         print(f"There was a problem exporting the notebook: {e}")
 
+
 def write_files(export_list, nb_node, file_name):
     """
-    export and write files from a notebook node
-    export_list(list of strings): name of valid nbconvert exporters
-    nb_node(nbformat node object): notebook to be my_converted
-    file_name(str): base name of file to be written
-    returns: None
-    """
+    Export and write files from a notebook node.
 
+    Args:
+        export_list (list of strings) -- name of valid nbconvert exporters
+        nb_node(nbformat node object) -- notebook to be my_converted
+        file_name (str) -- base name of file to be written
+
+    Returns:
+        None
+
+    """
     try:
         # export and write file.
         for export in export_list:
@@ -40,26 +51,33 @@ def write_files(export_list, nb_node, file_name):
 
             (body, resources) = exporter.from_notebook_node(nb_node)
             write_file = FilesWriter()
-            write_file.write(output=body, resources=resources, notebook_name=file_name)
+            write_file.write(
+                output=body,
+                resources=resources,
+                notebook_name=file_name
+            )
     except Exception as e:
         print(f"There was a problem exporting writing the file(s): {e}")
-
     return None
 
+
 def transform_notebooks(
-    ipynb_file="http://jakevdp.github.com/downloads/notebooks/XKCD_plots.ipynb",
+    ipynb_file,
     export_list=["html", "py"]
-    ):
+):
+    """
+    Transform notebooks to specified document types.
+
+    Args:
+
+        ipynb_file (str): url or path of Jupyter notebook to be converted
+        export_list(list of strs): output file types. Defaults: html, py.
+
+    Returns:
+        None
 
     """
-    Main module method. Creates transformed files on the operating system
-    Params:
-    ipynb_file (str): url or path of Jupyter notebook to be converted
-    export_list(list of strs): optons: html, py
-    Returns: None
-
-    """
-    file_name = ipynb_file.rsplit('/',1)[-1]
+    file_name = ipynb_file.rsplit('/', 1)[-1]
     file_name = file_name[:file_name.rfind('.')]
 
     nb_node = get_notebooks(ipynb_file)
--- tests/test_notebooktoall.py
+++ tests/test_notebooktoall.py
@@ -3,7 +3,6 @@
 
 """Tests for `notebooktoall` package."""
 import pytest
-import requests
 import os
 import nbformat
 from notebooktoall import notebooktoall as nba
@@ -11,7 +10,7 @@ from notebooktoall import notebooktoall as nba
 my_url = "http://jakevdp.github.com/downloads/notebooks/XKCD_plots.ipynb"
 # default test notebook address
 
-my_file = "my_test.ipynb"
+my_file = "my_tst.ipynb"
 # default test notebook file path
 
 my_file_types = ['html', 'py']
@@ -21,49 +20,48 @@ my_file_types = ['html', 'py']
 # def build_file():
 #    """test fixture to call function to create files from notebook"""
 
+
 # Integration tests
 def test_transform_notebooks():
-
+    """Test that output files are created when main function is called."""
     nba.transform_notebooks(ipynb_file=my_url, export_list=my_file_types)
-    file_name = my_url.rsplit('/',1)[-1]
+    file_name = my_url.rsplit('/', 1)[-1]
     file_base = file_name[:file_name.rfind('.')]
 
-    """check html file exists from url"""
+    """Check .html file exists from url."""
     assert os.path.exists(f'{file_base}.html')
 
-    """check .py file exists from url"""
+    """Check .py file exists from url."""
     assert os.path.exists(f'{file_base}.py')
-
 
     nba.transform_notebooks(ipynb_file=my_file, export_list=my_file_types)
-    file_name = my_file.rsplit('/',1)[-1]
+    file_name = my_file.rsplit('/', 1)[-1]
     file_base = file_name[:file_name.rfind('.')]
 
-    """check html file exists from path"""
+    """Check html file exists from path."""
     assert os.path.exists(f'{file_base}.html')
 
-    """check .py file exists from path"""
+    """Check .py file exists from path."""
     assert os.path.exists(f'{file_base}.py')
-
 
 
 # Unit tests
 def test_get_notebook():
-    """should return a NotebookNode object from a url"""
+    """Should return a NotebookNode object from a url."""
     my_nb_node = nba.get_notebooks(my_url)
     assert type(my_nb_node) is nbformat.notebooknode.NotebookNode
 
-    """should return a NotebookNode object from a path"""
+    """Should return a NotebookNode object from a path."""
     my_nb_node = nba.get_notebooks(my_file)
     assert type(my_nb_node) is nbformat.notebooknode.NotebookNode
 
-    """TODO error checking. Should raise an exception"""
-    bad_url = "http:/funn.yfdf"
+    """TODO error checking. Should raise an exception."""
+    # bad_url = "http:/funn.yfdf"
     # my_nb_node = get_notebook(my_url)
     # assert raises an error
 
 
 def test_write_files():
-    """should run to completion"""
+    """Should run to completion."""
     my_nb_node = nba.get_notebooks(my_url)
     nba.write_files(my_file_types, my_nb_node, file_name="my_file")
