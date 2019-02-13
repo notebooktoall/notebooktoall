d7280d1cab248a0f93282b3f95080c1877ca5e04
discdiver <jeffmshale@gmail.com>
2019-02-12 21:11:59 -0500

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/notebooktoall/notebooktoall',
-    version='0.1.3',
+    version='0.1.4',
     zip_safe=False,
 )
